fe35f7961e8b576f0a9b1eb509d64a7db16f808f
Gordon Clark <Gordon2108@gmail.com>
2016-10-25 15:20:02 -0700

    Requests approval to add item to meal.

--- CalCountMain.py
+++ CalCountMain.py
@@ -3,27 +3,28 @@
 # Date: Oct 10, 2016
 # Purpose: To count calories based on the input of fats, carbohydrates, and proteins
 cont ='y'
+keep_item = ''
+temp_cals = 0
 mealcals = 0
 itemcount = 0
 
 while cont.lower() == 'y':
-    # Input
+    #### Asks for Item nutrition facts and determines calories ####
     item_name = input("Enter item name >")
-
     g_carbs = int(input("carbs >"))
     g_fats = int(input("fats >"))
     g_protein = int(input("protein >"))
-
-    # Process
     cals = (g_carbs*4) + (g_fats*9) + (g_protein*4)
-
-    # Output
-    # print("Total calories for " + item_name + " are " + str(item_cals) )
-    # ^ dont use this method, use the below method.
+    ########
+    
+    ####Display item calories and verify it is to be added to the meal####
     print("Total calories for {} are {}.".format(item_name,cals) )
-    mealcals = mealcals + cals
-    itemcount = itemcount + 1
-    print("Your meal has {} items totalling {} calories.".format(itemcount,mealcals))
+    print("Your meal will have {} items totalling {} calories.".format(itemcount,mealcals))
+    keep_item = input("Do you want to add this item to your meal?(y/n)")
+    if keep_item.lower() == 'y':
+        mealcals = mealcals + cals
+        itemcount = itemcount + 1
     cont = input("Would you like to add another?")
 
+print("Your meal will have {} items totalling {} calories.".format(itemcount,mealcals))
 print("Thank you for using this program.")
