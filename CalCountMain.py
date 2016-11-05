55d56e3ad9f4c062d98b6c8c4555b695e4fe0d3e
Gordon Clark <Gordon2108@gmail.com>
2016-11-05 16:08:19 -0700

    Added try except error catching

--- CalCountMain.py
+++ CalCountMain.py
@@ -9,19 +9,52 @@ keep_item = ''
 cals = 0
 mealcals = 0
 itemcount = 0
+valid_data = False
 #### END OF VARIABLE DECLARATIONS ####
-
-
 
 #### MAIN LOOP ####
 while cont.lower() == 'y':
 
     ## ITEM PROPERTY -INPUT- ##
     # Asks the user about the food they wish to enter.
-    item_name = input("Enter item name >")
-    g_carbs = int(input("carbs >"))
-    g_fats = int(input("fats >"))
-    g_protein = int(input("protein >"))
+    while not valid_data:
+        item_name = input("Enter item name >")
+        if len(item_name) > 20:
+            print("We both know thats not a real food. Try again.")
+        elif len(item_name) == 0:
+            print("You entered nothing. Try again.")
+        else:
+            valid_data = True
+
+    # Flag Reset
+    valid_data = False
+
+    while not valid_data:
+        try:
+            g_carbs = int(input("carbs >"))
+            valid_data = True
+        except Exception as detail:
+            print("Error: ", detail)
+
+    # Flag Reset
+    valid_data = False
+
+    while not valid_data:
+        try:
+            g_fats = int(input("fats >"))
+            valid_data = True
+        except Exception as detail:
+            print("Error: ", detail)
+
+    # Flag Reset
+    valid_data = False
+
+    while not valid_data:
+        try:
+            g_protein = int(input("carbs >"))
+            valid_data = True
+        except Exception as detail:
+            print("Error: ", detail)
     ## END OF -INPUT- ##
 
     ## PROCESS & OUTPUT ##
@@ -41,7 +74,6 @@ while cont.lower() == 'y':
 
     # LOOP CONTINUE REQUEST #
     cont = input("Would you like to add another?")
-
 #### END OF MAIN LOOP ####
 
 
