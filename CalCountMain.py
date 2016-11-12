5e3b3741d2152176b0710b3ed0619c87c92ff1fe
Gordon Clark <Gordon2108@gmail.com>
2016-11-12 14:17:45 -0800

    Functions done! Code is beautiful.

--- CalCountMain.py
+++ CalCountMain.py
@@ -3,16 +3,15 @@
 # Date: Oct 10, 2016
 # Purpose: To count calories for a meal based on the input of fats, carbohydrates, and proteins.
 
-## VARIABLE DECLARATIONS ##
+# VARIABLE DECLARATIONS #
 cont = 'y'
 keep_item = ''
-cals = 0
 mealcals = 0
 itemcount = 0
 valid_data = False
 
 
-## FUNCTIONS ##
+# FUNCTIONS #
 def inp_item_name():  # Asks user for food name. Must be under 20 chars and more than 0
     while True:
         item_name = input("Enter item name >")
@@ -23,7 +22,7 @@ def inp_item_name():  # Asks user for food name. Must be under 20 chars and more
         return item_name
 
 
-def nut_input():  # Returns carbs, fats and proteins after asking for user input
+def nut_input():  # Returns carbs, fats and proteins as ints (in that order) after asking for user input
     while True:
         try:
             g_carbs = int(input("Carbs >"))
@@ -48,37 +47,32 @@ def nut_input():  # Returns carbs, fats and proteins after asking for user input
     return g_carbs, g_fats, g_protein
 
 
-def cal_calc(gytpe, quant):
+def cal_calc(gtype, quant):  # Takes f, c, or p string as gram type (fat, carbs, proteins) and returns calories
+    if gtype == 'f':
+        return quant * 9
+    else:
+        return quant * 4
 
 
-#### MAIN LOOP ####
+# Main Loop
 while cont.lower() == 'y':
 
-    item = inp_item_name()
+    item = inp_item_name()  # Gets the item name from the user
 
+    g_carbs, g_fats, g_protein = nut_input()  # Determines carbs, fats, and proteins in the item via user input
 
+    item_cals = cal_calc('c', g_carbs) + cal_calc('f', g_fats) + cal_calc('p', g_protein)
 
+    print("Total calories for {} are {}.".format(item, item_cals))
+    print("Your meal will have {} items totalling {} calories.".format(itemcount+1, (mealcals + item_cals)))
 
-
-    ## PROCESS & OUTPUT ##
-    # Calculates calories based on number of fats, protein, and carbs, and how many cals in each.
-    cals = (g_carbs * 4) + (g_fats * 9) + (g_protein * 4)
-    print("Total calories for {} are {}.".format(item_name, cals))
-    print("Your meal will have {} items totalling {} calories.".format(itemcount, (mealcals + cals)))
-    ## END PROCESS & OUTPUT ##
-
-
-    ## DATA VERIFICATION ##
-    # Verifies the user wishes to input this data.
     keep_item = input("Do you want to add this item to your meal?(y/n)")
     if keep_item.lower() == 'y':
-        mealcals += cals
+        mealcals += item_cals
         itemcount += 1
-    ## END DATA VERIFICATION
-
-    # LOOP CONTINUE REQUEST #
-    cont = input("Would you like to add another?")
-#### END OF MAIN LOOP ####
+        cont = input("Would you like to add another?")
+    else:
+        cont = input("The item has not been added to your meal. Would you like to add another item?")
 
 
 print("Your meal will have {} items totalling {} calories.".format(itemcount, mealcals))
